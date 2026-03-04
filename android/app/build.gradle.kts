520b7b7687fb1c76215e26760b21efa2abf4104d
williamjohanp <williamjohanp@gmail.com>
2026-03-04 14:00:06 +0700

    feat : add proguard-rules

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -31,12 +31,22 @@ android {
     }
 
     buildTypes {
-        release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
+            release {
             signingConfig = signingConfigs.getByName("debug")
+
+            isMinifyEnabled = true
+            isShrinkResources = true
+
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
         }
     }
+}
+
+dependencies {
+    implementation("com.google.mlkit:text-recognition:16.0.0")
 }
 
 flutter {
