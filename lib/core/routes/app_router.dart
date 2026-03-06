652a3820b72328d90d311a0d87237c3bc7b94518
Willjon <163702289+williamjohan@users.noreply.github.com>
2026-03-07 00:08:43 +0700

    fix : adjust app_router to handle home with blocprovider

--- lib/core/routes/app_router.dart
+++ lib/core/routes/app_router.dart
@@ -1,12 +1,13 @@
 // lib/core/routes/app_router.dart
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
+import '../../features/home/persentation/cubit/home_cubit.dart';
 import '../../features/init/presentation/cubit/init_cubit.dart';
 import '../../features/init/presentation/pages/splash_page.dart';
 import '../../features/vehicle_capture/domain/entities/vehicle_category.dart';
 import '../../features/vehicle_capture/presentation/cubit/vehicle_capture_cubit.dart';
 import '../../features/vehicle_capture/presentation/pages/capture_page.dart';
-import '../../features/vehicle_capture/presentation/pages/home_page.dart';
+import '../../features/home/persentation/pages/home_page.dart';
 import '../di/injection.dart';
 import 'app_routes.dart';
 
@@ -26,10 +27,15 @@ class AppRouter {
         },
       ),
 
-      // Rute Home Sungguhan
+      // Rute Home
       GoRoute(
         path: AppRoutes.home,
-        builder: (context, state) => const HomePage(),
+        builder: (context, state) {
+          return BlocProvider(
+            create: (_) => locator<HomeCubit>(), // Inject HomeCubit!
+            child: const HomePage(),
+          );
+        },
       ),
 
       // Rute Capture dengan path parameter /:category
