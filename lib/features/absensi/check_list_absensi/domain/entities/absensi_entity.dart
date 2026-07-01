0f90f1c9959ca6c5ea08cecaa7d2584fa7ea0c01
williamjohanp <williamjohanp@gmail.com>
2026-07-01 11:27:40 +0700

    feat : add absensi entity

---
+++ lib/features/absensi/check_list_absensi/domain/entities/absensi_entity.dart
@@ -0,0 +1,48 @@
+import 'package:equatable/equatable.dart';
+
+class AbsensiEntity extends Equatable {
+  final DateTime date;
+  final bool isPresent;
+  final double latitude;
+  final double longitude;
+  final AbsensiCheckList? checkList;
+
+  const AbsensiEntity({
+    required this.date,
+    required this.isPresent,
+    required this.latitude,
+    required this.longitude,
+    this.checkList,
+  });
+
+  @override
+  List<Object?> get props => [date, isPresent, latitude, longitude, checkList];
+}
+
+class AbsensiCheckList extends Equatable {
+  final bool edc;
+  final bool qrisRompi;
+  final bool cctv;
+  final bool tsPark;
+  final int totalMotor;
+  final int totalMobil;
+
+  const AbsensiCheckList({
+    required this.edc,
+    required this.qrisRompi,
+    required this.cctv,
+    required this.tsPark,
+    required this.totalMotor,
+    required this.totalMobil,
+  });
+
+  @override
+  List<Object?> get props => [
+    edc,
+    qrisRompi,
+    cctv,
+    tsPark,
+    totalMotor,
+    totalMobil,
+  ];
+}
