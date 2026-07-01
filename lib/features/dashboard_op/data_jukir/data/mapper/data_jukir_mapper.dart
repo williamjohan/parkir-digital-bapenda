6af8bc517256f78ecec4d044ccf6ba0eda411690
Sinta Ayu Rismawati <sintaayr03@gmail.com>
2026-07-01 10:47:09 +0700

    fix : hapus card jukir di transaction & revisi data-domain-presentasion data jukir

--- lib/features/dashboard_op/data_jukir/data/mapper/data_jukir_mapper.dart
+++ lib/features/dashboard_op/data_jukir/data/mapper/data_jukir_mapper.dart
@@ -4,14 +4,23 @@ import 'package:parkir_digital_bapenda/features/dashboard_op/data_jukir/domain/e
 class DataJukirMapper {
   static DataJukirEntity toEntity(DataJukirModel model) {
     return DataJukirEntity(
-      nop: model.nop,
-      username: model.username,
-      deviceId: model.idDevice,
-      namaPetugas: model.namaPetugas,
+      idDevice: model.idDevice,
+      petugas: model.petugas,
       shift: model.shift,
-      // totalPendapatan: model.totalPendapatan,
-      // totalMobil: model.totalMobil,
-      // totalMotor: model.totalMotor,
+      totalMobilHariIni: model.totalMobilHariIni,
+      totalMotorHariIni: model.totalMotorHariIni,
+      totalNominalMobilHariIni: model.totalNominalMobilHariIni,
+      totalNominalMotorHariIni: model.totalNominalMotorHariIni,
+      totalKendaraan: model.totalKendaraan,
+      totalNominal: model.totalNominal,
+      usernameList: model.usernameList
+          .map(
+            (e) => UsernameEntity(
+              username: e.username,
+              namaPetugas: e.namaPetugas,
+            ),
+          )
+          .toList(),
     );
   }
 }
--- lib/features/dashboard_op/data_jukir/domain/entities/data_jukir_entity.freezed.dart
+++ lib/features/dashboard_op/data_jukir/domain/entities/data_jukir_entity.freezed.dart
@@ -17,11 +17,16 @@ final _privateConstructorUsedError = UnsupportedError(
 
 /// @nodoc
 mixin _$DataJukirEntity {
-  String get nop => throw _privateConstructorUsedError;
-  String get username => throw _privateConstructorUsedError;
-  String get deviceId => throw _privateConstructorUsedError;
-  String get namaPetugas => throw _privateConstructorUsedError;
+  String get idDevice => throw _privateConstructorUsedError;
+  String get petugas => throw _privateConstructorUsedError;
   String get shift => throw _privateConstructorUsedError;
+  int get totalMobilHariIni => throw _privateConstructorUsedError;
+  int get totalMotorHariIni => throw _privateConstructorUsedError;
+  int get totalNominalMobilHariIni => throw _privateConstructorUsedError;
+  int get totalNominalMotorHariIni => throw _privateConstructorUsedError;
+  int get totalKendaraan => throw _privateConstructorUsedError;
+  int get totalNominal => throw _privateConstructorUsedError;
+  List<UsernameEntity> get usernameList => throw _privateConstructorUsedError;
 
   /// Create a copy of DataJukirEntity
   /// with the given fields replaced by the non-null parameter values.
@@ -38,11 +43,16 @@ abstract class $DataJukirEntityCopyWith<$Res> {
   ) = _$DataJukirEntityCopyWithImpl<$Res, DataJukirEntity>;
   @useResult
   $Res call({
-    String nop,
-    String username,
-    String deviceId,
-    String namaPetugas,
+    String idDevice,
+    String petugas,
     String shift,
+    int totalMobilHariIni,
+    int totalMotorHariIni,
+    int totalNominalMobilHariIni,
+    int totalNominalMotorHariIni,
+    int totalKendaraan,
+    int totalNominal,
+    List<UsernameEntity> usernameList,
   });
 }
 
@@ -61,34 +71,59 @@ class _$DataJukirEntityCopyWithImpl<$Res, $Val extends DataJukirEntity>
   @pragma('vm:prefer-inline')
   @override
   $Res call({
-    Object? nop = null,
-    Object? username = null,
-    Object? deviceId = null,
-    Object? namaPetugas = null,
+    Object? idDevice = null,
+    Object? petugas = null,
     Object? shift = null,
+    Object? totalMobilHariIni = null,
+    Object? totalMotorHariIni = null,
+    Object? totalNominalMobilHariIni = null,
+    Object? totalNominalMotorHariIni = null,
+    Object? totalKendaraan = null,
+    Object? totalNominal = null,
+    Object? usernameList = null,
   }) {
     return _then(
       _value.copyWith(
-            nop: null == nop
-                ? _value.nop
-                : nop // ignore: cast_nullable_to_non_nullable
+            idDevice: null == idDevice
+                ? _value.idDevice
+                : idDevice // ignore: cast_nullable_to_non_nullable
                       as String,
-            username: null == username
-                ? _value.username
-                : username // ignore: cast_nullable_to_non_nullable
-                      as String,
-            deviceId: null == deviceId
-                ? _value.deviceId
-                : deviceId // ignore: cast_nullable_to_non_nullable
-                      as String,
-            namaPetugas: null == namaPetugas
-                ? _value.namaPetugas
-                : namaPetugas // ignore: cast_nullable_to_non_nullable
+            petugas: null == petugas
+                ? _value.petugas
+                : petugas // ignore: cast_nullable_to_non_nullable
                       as String,
             shift: null == shift
                 ? _value.shift
                 : shift // ignore: cast_nullable_to_non_nullable
                       as String,
+            totalMobilHariIni: null == totalMobilHariIni
+                ? _value.totalMobilHariIni
+                : totalMobilHariIni // ignore: cast_nullable_to_non_nullable
+                      as int,
+            totalMotorHariIni: null == totalMotorHariIni
+                ? _value.totalMotorHariIni
+                : totalMotorHariIni // ignore: cast_nullable_to_non_nullable
+                      as int,
+            totalNominalMobilHariIni: null == totalNominalMobilHariIni
+                ? _value.totalNominalMobilHariIni
+                : totalNominalMobilHariIni // ignore: cast_nullable_to_non_nullable
+                      as int,
+            totalNominalMotorHariIni: null == totalNominalMotorHariIni
+                ? _value.totalNominalMotorHariIni
+                : totalNominalMotorHariIni // ignore: cast_nullable_to_non_nullable
+                      as int,
+            totalKendaraan: null == totalKendaraan
+                ? _value.totalKendaraan
+                : totalKendaraan // ignore: cast_nullable_to_non_nullable
+                      as int,
+            totalNominal: null == totalNominal
+                ? _value.totalNominal
+                : totalNominal // ignore: cast_nullable_to_non_nullable
+                      as int,
+            usernameList: null == usernameList
+                ? _value.usernameList
+                : usernameList // ignore: cast_nullable_to_non_nullable
+                      as List<UsernameEntity>,
           )
           as $Val,
     );
@@ -105,11 +140,16 @@ abstract class _$$DataJukirEntityImplCopyWith<$Res>
   @override
   @useResult
   $Res call({
-    String nop,
-    String username,
-    String deviceId,
-    String namaPetugas,
+    String idDevice,
+    String petugas,
     String shift,
+    int totalMobilHariIni,
+    int totalMotorHariIni,
+    int totalNominalMobilHariIni,
+    int totalNominalMotorHariIni,
+    int totalKendaraan,
+    int totalNominal,
+    List<UsernameEntity> usernameList,
   });
 }
 
@@ -127,34 +167,59 @@ class __$$DataJukirEntityImplCopyWithImpl<$Res>
   @pragma('vm:prefer-inline')
   @override
   $Res call({
-    Object? nop = null,
-    Object? username = null,
-    Object? deviceId = null,
-    Object? namaPetugas = null,
+    Object? idDevice = null,
+    Object? petugas = null,
     Object? shift = null,
+    Object? totalMobilHariIni = null,
+    Object? totalMotorHariIni = null,
+    Object? totalNominalMobilHariIni = null,
+    Object? totalNominalMotorHariIni = null,
+    Object? totalKendaraan = null,
+    Object? totalNominal = null,
+    Object? usernameList = null,
   }) {
     return _then(
       _$DataJukirEntityImpl(
-        nop: null == nop
-            ? _value.nop
-            : nop // ignore: cast_nullable_to_non_nullable
+        idDevice: null == idDevice
+            ? _value.idDevice
+            : idDevice // ignore: cast_nullable_to_non_nullable
                   as String,
-        username: null == username
-            ? _value.username
-            : username // ignore: cast_nullable_to_non_nullable
-                  as String,
-        deviceId: null == deviceId
-            ? _value.deviceId
-            : deviceId // ignore: cast_nullable_to_non_nullable
-                  as String,
-        namaPetugas: null == namaPetugas
-            ? _value.namaPetugas
-            : namaPetugas // ignore: cast_nullable_to_non_nullable
+        petugas: null == petugas
+            ? _value.petugas
+            : petugas // ignore: cast_nullable_to_non_nullable
                   as String,
         shift: null == shift
             ? _value.shift
             : shift // ignore: cast_nullable_to_non_nullable
                   as String,
+        totalMobilHariIni: null == totalMobilHariIni
+            ? _value.totalMobilHariIni
+            : totalMobilHariIni // ignore: cast_nullable_to_non_nullable
+                  as int,
+        totalMotorHariIni: null == totalMotorHariIni
+            ? _value.totalMotorHariIni
+            : totalMotorHariIni // ignore: cast_nullable_to_non_nullable
+                  as int,
+        totalNominalMobilHariIni: null == totalNominalMobilHariIni
+            ? _value.totalNominalMobilHariIni
+            : totalNominalMobilHariIni // ignore: cast_nullable_to_non_nullable
+                  as int,
+        totalNominalMotorHariIni: null == totalNominalMotorHariIni
+            ? _value.totalNominalMotorHariIni
+            : totalNominalMotorHariIni // ignore: cast_nullable_to_non_nullable
+                  as int,
+        totalKendaraan: null == totalKendaraan
+            ? _value.totalKendaraan
+            : totalKendaraan // ignore: cast_nullable_to_non_nullable
+                  as int,
+        totalNominal: null == totalNominal
+            ? _value.totalNominal
+            : totalNominal // ignore: cast_nullable_to_non_nullable
+                  as int,
+        usernameList: null == usernameList
+            ? _value._usernameList
+            : usernameList // ignore: cast_nullable_to_non_nullable
+                  as List<UsernameEntity>,
       ),
     );
   }
@@ -164,27 +229,47 @@ class __$$DataJukirEntityImplCopyWithImpl<$Res>
 
 class _$DataJukirEntityImpl implements _DataJukirEntity {
   const _$DataJukirEntityImpl({
-    required this.nop,
-    required this.username,
-    required this.deviceId,
-    required this.namaPetugas,
+    required this.idDevice,
+    required this.petugas,
     required this.shift,
-  });
+    required this.totalMobilHariIni,
+    required this.totalMotorHariIni,
+    required this.totalNominalMobilHariIni,
+    required this.totalNominalMotorHariIni,
+    required this.totalKendaraan,
+    required this.totalNominal,
+    required final List<UsernameEntity> usernameList,
+  }) : _usernameList = usernameList;
 
   @override
-  final String nop;
+  final String idDevice;
   @override
-  final String username;
-  @override
-  final String deviceId;
-  @override
-  final String namaPetugas;
+  final String petugas;
   @override
   final String shift;
+  @override
+  final int totalMobilHariIni;
+  @override
+  final int totalMotorHariIni;
+  @override
+  final int totalNominalMobilHariIni;
+  @override
+  final int totalNominalMotorHariIni;
+  @override
+  final int totalKendaraan;
+  @override
+  final int totalNominal;
+  final List<UsernameEntity> _usernameList;
+  @override
+  List<UsernameEntity> get usernameList {
+    if (_usernameList is EqualUnmodifiableListView) return _usernameList;
+    // ignore: implicit_dynamic_type
+    return EqualUnmodifiableListView(_usernameList);
+  }
 
   @override
   String toString() {
-    return 'DataJukirEntity(nop: $nop, username: $username, deviceId: $deviceId, namaPetugas: $namaPetugas, shift: $shift)';
+    return 'DataJukirEntity(idDevice: $idDevice, petugas: $petugas, shift: $shift, totalMobilHariIni: $totalMobilHariIni, totalMotorHariIni: $totalMotorHariIni, totalNominalMobilHariIni: $totalNominalMobilHariIni, totalNominalMotorHariIni: $totalNominalMotorHariIni, totalKendaraan: $totalKendaraan, totalNominal: $totalNominal, usernameList: $usernameList)';
   }
 
   @override
@@ -192,19 +277,48 @@ class _$DataJukirEntityImpl implements _DataJukirEntity {
     return identical(this, other) ||
         (other.runtimeType == runtimeType &&
             other is _$DataJukirEntityImpl &&
-            (identical(other.nop, nop) || other.nop == nop) &&
-            (identical(other.username, username) ||
-                other.username == username) &&
-            (identical(other.deviceId, deviceId) ||
-                other.deviceId == deviceId) &&
-            (identical(other.namaPetugas, namaPetugas) ||
-                other.namaPetugas == namaPetugas) &&
-            (identical(other.shift, shift) || other.shift == shift));
+            (identical(other.idDevice, idDevice) ||
+                other.idDevice == idDevice) &&
+            (identical(other.petugas, petugas) || other.petugas == petugas) &&
+            (identical(other.shift, shift) || other.shift == shift) &&
+            (identical(other.totalMobilHariIni, totalMobilHariIni) ||
+                other.totalMobilHariIni == totalMobilHariIni) &&
+            (identical(other.totalMotorHariIni, totalMotorHariIni) ||
+                other.totalMotorHariIni == totalMotorHariIni) &&
+            (identical(
+                  other.totalNominalMobilHariIni,
+                  totalNominalMobilHariIni,
+                ) ||
+                other.totalNominalMobilHariIni == totalNominalMobilHariIni) &&
+            (identical(
+                  other.totalNominalMotorHariIni,
+                  totalNominalMotorHariIni,
+                ) ||
+                other.totalNominalMotorHariIni == totalNominalMotorHariIni) &&
+            (identical(other.totalKendaraan, totalKendaraan) ||
+                other.totalKendaraan == totalKendaraan) &&
+            (identical(other.totalNominal, totalNominal) ||
+                other.totalNominal == totalNominal) &&
+            const DeepCollectionEquality().equals(
+              other._usernameList,
+              _usernameList,
+            ));
   }
 
   @override
-  int get hashCode =>
-      Object.hash(runtimeType, nop, username, deviceId, namaPetugas, shift);
+  int get hashCode => Object.hash(
+    runtimeType,
+    idDevice,
+    petugas,
+    shift,
+    totalMobilHariIni,
+    totalMotorHariIni,
+    totalNominalMobilHariIni,
+    totalNominalMotorHariIni,
+    totalKendaraan,
+    totalNominal,
+    const DeepCollectionEquality().hash(_usernameList),
+  );
 
   /// Create a copy of DataJukirEntity
   /// with the given fields replaced by the non-null parameter values.
@@ -220,28 +334,200 @@ class _$DataJukirEntityImpl implements _DataJukirEntity {
 
 abstract class _DataJukirEntity implements DataJukirEntity {
   const factory _DataJukirEntity({
-    required final String nop,
-    required final String username,
-    required final String deviceId,
-    required final String namaPetugas,
+    required final String idDevice,
+    required final String petugas,
     required final String shift,
+    required final int totalMobilHariIni,
+    required final int totalMotorHariIni,
+    required final int totalNominalMobilHariIni,
+    required final int totalNominalMotorHariIni,
+    required final int totalKendaraan,
+    required final int totalNominal,
+    required final List<UsernameEntity> usernameList,
   }) = _$DataJukirEntityImpl;
 
   @override
-  String get nop;
+  String get idDevice;
   @override
-  String get username;
-  @override
-  String get deviceId;
-  @override
-  String get namaPetugas;
+  String get petugas;
   @override
   String get shift;
+  @override
+  int get totalMobilHariIni;
+  @override
+  int get totalMotorHariIni;
+  @override
+  int get totalNominalMobilHariIni;
+  @override
+  int get totalNominalMotorHariIni;
+  @override
+  int get totalKendaraan;
+  @override
+  int get totalNominal;
+  @override
+  List<UsernameEntity> get usernameList;
 
   /// Create a copy of DataJukirEntity
   /// with the given fields replaced by the non-null parameter values.
   @override
   @JsonKey(includeFromJson: false, includeToJson: false)
   _$$DataJukirEntityImplCopyWith<_$DataJukirEntityImpl> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+mixin _$UsernameEntity {
+  String get username => throw _privateConstructorUsedError;
+  String get namaPetugas => throw _privateConstructorUsedError;
+
+  /// Create a copy of UsernameEntity
+  /// with the given fields replaced by the non-null parameter values.
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  $UsernameEntityCopyWith<UsernameEntity> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $UsernameEntityCopyWith<$Res> {
+  factory $UsernameEntityCopyWith(
+    UsernameEntity value,
+    $Res Function(UsernameEntity) then,
+  ) = _$UsernameEntityCopyWithImpl<$Res, UsernameEntity>;
+  @useResult
+  $Res call({String username, String namaPetugas});
+}
+
+/// @nodoc
+class _$UsernameEntityCopyWithImpl<$Res, $Val extends UsernameEntity>
+    implements $UsernameEntityCopyWith<$Res> {
+  _$UsernameEntityCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  /// Create a copy of UsernameEntity
+  /// with the given fields replaced by the non-null parameter values.
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({Object? username = null, Object? namaPetugas = null}) {
+    return _then(
+      _value.copyWith(
+            username: null == username
+                ? _value.username
+                : username // ignore: cast_nullable_to_non_nullable
+                      as String,
+            namaPetugas: null == namaPetugas
+                ? _value.namaPetugas
+                : namaPetugas // ignore: cast_nullable_to_non_nullable
+                      as String,
+          )
+          as $Val,
+    );
+  }
+}
+
+/// @nodoc
+abstract class _$$UsernameEntityImplCopyWith<$Res>
+    implements $UsernameEntityCopyWith<$Res> {
+  factory _$$UsernameEntityImplCopyWith(
+    _$UsernameEntityImpl value,
+    $Res Function(_$UsernameEntityImpl) then,
+  ) = __$$UsernameEntityImplCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call({String username, String namaPetugas});
+}
+
+/// @nodoc
+class __$$UsernameEntityImplCopyWithImpl<$Res>
+    extends _$UsernameEntityCopyWithImpl<$Res, _$UsernameEntityImpl>
+    implements _$$UsernameEntityImplCopyWith<$Res> {
+  __$$UsernameEntityImplCopyWithImpl(
+    _$UsernameEntityImpl _value,
+    $Res Function(_$UsernameEntityImpl) _then,
+  ) : super(_value, _then);
+
+  /// Create a copy of UsernameEntity
+  /// with the given fields replaced by the non-null parameter values.
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({Object? username = null, Object? namaPetugas = null}) {
+    return _then(
+      _$UsernameEntityImpl(
+        username: null == username
+            ? _value.username
+            : username // ignore: cast_nullable_to_non_nullable
+                  as String,
+        namaPetugas: null == namaPetugas
+            ? _value.namaPetugas
+            : namaPetugas // ignore: cast_nullable_to_non_nullable
+                  as String,
+      ),
+    );
+  }
+}
+
+/// @nodoc
+
+class _$UsernameEntityImpl implements _UsernameEntity {
+  const _$UsernameEntityImpl({
+    required this.username,
+    required this.namaPetugas,
+  });
+
+  @override
+  final String username;
+  @override
+  final String namaPetugas;
+
+  @override
+  String toString() {
+    return 'UsernameEntity(username: $username, namaPetugas: $namaPetugas)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$UsernameEntityImpl &&
+            (identical(other.username, username) ||
+                other.username == username) &&
+            (identical(other.namaPetugas, namaPetugas) ||
+                other.namaPetugas == namaPetugas));
+  }
+
+  @override
+  int get hashCode => Object.hash(runtimeType, username, namaPetugas);
+
+  /// Create a copy of UsernameEntity
+  /// with the given fields replaced by the non-null parameter values.
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$UsernameEntityImplCopyWith<_$UsernameEntityImpl> get copyWith =>
+      __$$UsernameEntityImplCopyWithImpl<_$UsernameEntityImpl>(
+        this,
+        _$identity,
+      );
+}
+
+abstract class _UsernameEntity implements UsernameEntity {
+  const factory _UsernameEntity({
+    required final String username,
+    required final String namaPetugas,
+  }) = _$UsernameEntityImpl;
+
+  @override
+  String get username;
+  @override
+  String get namaPetugas;
+
+  /// Create a copy of UsernameEntity
+  /// with the given fields replaced by the non-null parameter values.
+  @override
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  _$$UsernameEntityImplCopyWith<_$UsernameEntityImpl> get copyWith =>
       throw _privateConstructorUsedError;
 }
